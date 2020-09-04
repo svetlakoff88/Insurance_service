8c634b955a03f6d2a9ef26e2d83bb0c43c07c011
vyacheslav <svetlakoff88@gmail.com>
2020-09-04 15:19:16 +0300

    update 4.09

--- main.py
+++ main.py
@@ -3,10 +3,11 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import datetime
-from calendar import isleap
 import numpy as np
+from calendar import isleap
+import datetime
 from sklearn.linear_model import LinearRegression, Ridge
+from random import random, choice
 
 data = 'data_insurance.xlsx'
 
@@ -21,7 +22,7 @@ def get_shape(source):
 # get_shape(data)
 
 # формирование списков сумм продаж и списки месяцев
-def get_data_from_source(source, year):
+def get_data_from_source(source):
     dates17 = list()
     dates18 = list()
     dates19 = list()
@@ -48,36 +49,66 @@ def get_data_from_source(source, year):
         else:
             dates19.append(year19[k])
     # return visualisation(list(series17+series18+series19), list(dates17+dates18+dates19), year)
-    return date_prepend(source, list(series17+series18+series19), list(dates17+dates18+dates19))
+    return date_prepend(wb, list(series17+series18+series19), list(dates17+dates18+dates19))
 
 
 def date_prepend(source, series, dates):
-    window = 365
-    horizon = 365
-    df = pd.read_excel(source)
-    count = df.shape[0]
+    window = 12
+    horizon = 2
+    count = source.shape[0]
     count_tr = count-horizon
     mas_x = list()
     mas_y = list()
-    for i in range(0, count_tr-window):
+    for i in range(0, count_tr-window+1):
         mas_x.append(series[i:(i+window)])
         mas_y.append(series[i+window])
     x = np.array(mas_x)
     y = np.array(mas_y)
-    return linear_regression_object(x, y)
+    # return ridge_object(x, y)
+    # return ridge_alpha_value(arr_x=x, arr_y=y)
+    return ridge_object(x, y)
 
 
 def linear_regression_object(arr_x, arr_y):
+    """Функция тестового создания и обучения объекта алгоритмом Линейной регрессии"""
     regression_obj = LinearRegression().fit(arr_x, arr_y)
-    return ridge_object(regression_obj, arr_x, arr_y)
+    return print(regression_obj.score(arr_x, arr_y))
 
 
-def ridge_object(obj, arr_x, arr_y):
-    obj = Ridge(normalize=True, alpha=0.9)
+def ridge_alpha_value(arr_x, arr_y):
+    """Функция для формирования тестового массива коэффициентов"""
+    score_lst = list()
+    alpha_lst = [random().__round__(2) for i in range(10)]
+    for j in alpha_lst:
+        reg = Ridge(normalize=True, alpha=j)
+        reg.fit(arr_x, arr_y)
+        score_lst.append(reg.score(arr_x, arr_y))
+    return print(alpha_lst)
+
+
+def ridge_object(arr_x, arr_y):
+    alpha_lst = [random().__round__(2) for i in range(10)]
+    obj = Ridge(normalize=True, alpha=choice(alpha_lst))
     obj.fit(arr_x, arr_y)
-    return obj
+    return print(obj.score(arr_x, arr_y))
 
-"""def get_dates_list(series, year):
+
+def predict_data_get(pre_fit_obj, horizon, window, count_tr, series):
+    # j = 0
+    rez_m = list()
+    for j in range(horizon):
+        s = np.copy(series[count_tr-window + j:count_tr])
+        if len(rez_m) > 0:
+            for i in range(j):
+                s = np.append(s, rez_m[i])
+        ss = list()
+        ss.append(np.array(s))
+        rez = pre_fit_obj.predict(ss)
+        rez_m.append(rez[0])
+    return print(rez_m)
+
+
+def get_dates_list(series, year):
     dates = set()
     start = datetime.date(year, 12, 31)
     if isleap(year):
@@ -87,13 +118,13 @@ def ridge_object(obj, arr_x, arr_y):
     for i in range(0, period):
         dates.add((start-datetime.timedelta(days=i)).__format__('%m.%Y'))
     return visualisation(series, sorted(dates), year)
-"""
 
-# вывод графика
+
 def visualisation(series, dates, year):
+    """Функция отрисовки графика"""
     plt.plot(dates, series, color='red', linestyle='solid', label=year)
     plt.legend(loc=9)
     return plt.show()
 
 
-get_data_from_source(data, '2017-2019')
+get_data_from_source(data)
